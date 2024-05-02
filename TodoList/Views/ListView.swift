973cc6a9bc42ac9e36b0e0f24e413b26ef843b51
x43125 <wx43125@163.com>
2024-05-02 15:23:33 +0800

    add add button

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,23 +9,28 @@ import SwiftUI
 
 struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
-
     
     var body: some View {
         // 列表
-        List {
-            // for循环 类似 v-for
-            ForEach(listViewModel.items) { item in
-                // 对子view的引用
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                Text("No items")
+            } else {
+                List {
+                    // for循环 类似 v-for
+                    ForEach(listViewModel.items) { item in
+                        // 对子view的引用
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
         // 标题
         .navigationTitle("Todo List 📋")
@@ -36,13 +41,14 @@ struct ListView: View {
             // 尾
             trailing:
                 // 跳转到新的页面
-                NavigationLink(
-                    // 展示内容
-                    "Add",
-                    // 实际的跳转内容
-                    destination: AddView()
-                )
+            NavigationLink(
+                // 展示内容
+                "Add",
+                // 实际的跳转内容
+                destination: AddView()
+            )
         )
+        
     }
 }
 
---
+++ TodoList/Views/NoItemView.swift
@@ -0,0 +1,70 @@
+//
+//  NoItemView.swift
+//  TodoList
+//
+//  Created by 王详 on 2024/4/18.
+//
+
+import SwiftUI
+
+struct NoItemView: View {
+    
+    @State var animate: Bool = false
+    var body: some View { 
+        ScrollView {
+            VStack(spacing: 10){
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a person?Are you a person?Are you a person?Are you a person?Are you a person?Are you a person?Are you a person?Are you a person?Are you a person?")
+                    .padding(.bottom, 20)
+                NavigationLink (
+                    destination: AddView(),
+                    label: {
+                    Text("Add Sth!")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? Color.red : Color.accentColor)
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? Color.red.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0.0,
+                    y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else {return}
+        DispatchQueue.main.asyncAfter(deadline: .now()+1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+struct NoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            NoItemView()
+                .navigationTitle("Title")
+        }
+        
+    }
+}
